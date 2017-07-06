4ebd3ee65b3edf38070c44dc5f90380f623dedd1
王肖 <wangxiao@wangxiaodeMacBook-Air.local>
2017-07-06 10:43:07 +0800

    完善bs4使用例子

--- test_bs4.py
+++ test_bs4.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
+import re
 
 html_doc = """
 <html><head><title>The Dormouse's story</title></head>
@@ -23,3 +24,20 @@ print '获取所有信息'
 links = soup.find_all('a')
 for link in links:
 	print link.name, link['href'], link.get_text()
+
+print '获取lacie链接'
+link_node = soup.find('a', href="http://example.com/lacie")
+print link_node.name, link_node['href'], link_node.get_text()
+
+
+print '使用正则表达式'
+link_re = soup.find('a', href=re.compile(r"ill"))
+print link_re.name, link_re['href'], link_re.get_text()
+
+print '获取p文字'
+p_node = soup.find('p', class_="title")
+print p_node.name, p_node.get_text()
+
+
+
+
